64ed18965f40c0775e8a742cdd6e58d7ee424994
Jony Karki <johnnykarki0@gmail.com>
2017-09-26 12:58:04 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :search_suggestions
   devise_for :users
   root 'pages#index'
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925093640) do
+ActiveRecord::Schema.define(version: 20170926113817) do
+
+  create_table "search_suggestions", force: :cascade do |t|
+    t.string   "term"
+    t.integer  "popularity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
